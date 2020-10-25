66e3fa4ee463d3d760c8cd9143bf9e79dab18176
zengke <superisaac.ke@gmail.com>
2020-10-25 16:01:58 +0800

    more testing

--- tube/router_test.go
+++ tube/router_test.go
@@ -1,9 +1,10 @@
 package tube
 
 import (
+	"encoding/json"
 	"github.com/stretchr/testify/assert"
 	"testing"
-	//	jsonrpc "github.com/superisaac/rpctube/jsonrpc"
+	jsonrpc "github.com/superisaac/rpctube/jsonrpc"
 )
 
 // implements ConnT
@@ -32,4 +33,35 @@ func TestJoinConn(t *testing.T) {
 	methods := router.GetMethods(cid)
 	assert.Equal(1, len(methods))
 	assert.Equal("abc", methods[0])
+}
+
+
+func TestRouteMessage(t *testing.T) {
+	assert := assert.New(t)
+	router := NewRouter()
+
+	cid := CID(1002)
+	ch := make(MsgChannel, 100)
+	router.Join(cid, ch)
+	assert.Equal(1, len(router.ConnMap))
+	router.RegisterMethod(cid, "abc")
+
+	cid1 := CID(1003)
+	ch1 := make(MsgChannel, 100)
+	router.Join(cid1, ch1)
+
+	j1 := `{
+"id": 100002,
+"method": "abc",
+"params": [1, 3]
+}`
+	
+	msg, err := jsonrpc.ParseMessage([]byte(j1))
+	assert.Nil(err)
+	assert.Equal(json.Number("100002"), msg.Id)
+	router.RouteMessage(msg, cid1)
+
+	rcvmsg := <- ch
+	assert.Equal(msg.Id, rcvmsg.Id)
+	assert.True(rcvmsg.IsRequest())
 }
