d299942587a6d87e70b9c1a812a1f5a33d19c55c
Zeng Ke <superisaac.ke@gmail.com>
2020-12-13 11:56:46 +0800

    fix json.Number

--- jsonrpc/validators.go
+++ jsonrpc/validators.go
@@ -5,11 +5,11 @@ import (
 )
 
 
-func ValidateNumber(v interface{}) (*json.Number, error) {
-	if n, ok := v.(*json.Number); ok {
+func ValidateNumber(v interface{}) (json.Number, error) {
+	if n, ok := v.(json.Number); ok {
 		return n, nil
 	} else {
-		return nil, &RPCError{10400, "require a number", false}
+		return json.Number("0"), &RPCError{10400, "require a number", false}
 	}
 }
 
