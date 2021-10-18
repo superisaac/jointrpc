21fd5d423d2b46c11a8656d34a9cc42dfc46e709
Zeng Ke <superisaac.ke@gmail.com>
2021-10-18 11:40:29 +0800

    server flag.go

--- client/client.go
+++ client/client.go
@@ -3,7 +3,6 @@ package client
 import (
 	//"context"
 	//"errors"
-	"flag"
 	"fmt"
 	//grpc_retry "github.com/grpc-ecosystem/go-grpc-middleware/retry"
 	log "github.com/sirupsen/logrus"
@@ -11,7 +10,6 @@ import (
 	//jsonrpc "github.com/superisaac/jointrpc/jsonrpc"
 	//"io"
 	"net/url"
-	"os"
 	//"time"
 	//server "github.com/superisaac/jointrpc/server"
 	//"github.com/superisaac/jointrpc/dispatch"
@@ -143,33 +141,3 @@ func (self *RPCClient) CheckStatus(status *intf.Status, methodName string) error
 	}
 }
 
-// misc functions
-func NewServerFlag(flagSet *flag.FlagSet) *ServerFlag {
-	seflag := new(ServerFlag)
-	seflag.pAddress = flagSet.String("c", "", "the tube server address")
-	seflag.pCertFile = flagSet.String("cert", "", "the cert file, default empty")
-	return seflag
-}
-
-func (self *ServerFlag) ptrValue() ServerEntry {
-	return ServerEntry{
-		ServerUrl: *self.pAddress,
-		CertFile:  *self.pCertFile,
-	}
-}
-
-func (self *ServerFlag) Get() ServerEntry {
-	value := self.ptrValue()
-	if value.ServerUrl == "" {
-		value.ServerUrl = os.Getenv("TUBE_CONNECT")
-	}
-
-	if value.ServerUrl == "" {
-		value.ServerUrl = "h2c://localhost:50055"
-	}
-
-	if value.CertFile == "" {
-		value.CertFile = os.Getenv("TUBE_CONNECT")
-	}
-	return value
-}
---
+++ client/serverflag.go
@@ -0,0 +1,37 @@
+package client
+
+import (
+	"os"
+	"flag"
+)
+	
+// misc functions
+func NewServerFlag(flagSet *flag.FlagSet) *ServerFlag {
+	seflag := new(ServerFlag)
+	seflag.pAddress = flagSet.String("c", "", "the tube server address")
+	seflag.pCertFile = flagSet.String("cert", "", "the cert file, default empty")
+	return seflag
+}
+
+func (self *ServerFlag) ptrValue() ServerEntry {
+	return ServerEntry{
+		ServerUrl: *self.pAddress,
+		CertFile:  *self.pCertFile,
+	}
+}
+
+func (self *ServerFlag) Get() ServerEntry {
+	value := self.ptrValue()
+	if value.ServerUrl == "" {
+		value.ServerUrl = os.Getenv("TUBE_CONNECT")
+	}
+
+	if value.ServerUrl == "" {
+		value.ServerUrl = "h2c://localhost:50055"
+	}
+
+	if value.CertFile == "" {
+		value.CertFile = os.Getenv("TUBE_CONNECT")
+	}
+	return value
+}
